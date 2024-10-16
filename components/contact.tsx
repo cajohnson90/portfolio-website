83cde28314c33fde102cbe6083f1ea9af1d2b41e
Cameron Johnson <cameronjohnson@Camerons-MacBook-Pro.local>
2024-10-15 22:21:51 -0700

    added the contacts section

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,23 +1,50 @@
 "use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import useSectionInView from "@/lib/hooks";
+import { FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
 
   return (
     <motion.section
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
+      className="mb-20 w-[min(100%, 38rem)] text-center sm:mb-28 scroll-mt-28"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
       id="contact"
       ref={ref}
     >
       <SectionHeading children="Contact" />
-      <h3 className="text-3xl font-medium mb-8">Work In Progress</h3>
+      <p className="text-gray-500 -mt-5">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:xxcameronjohnson@outlook.com">
+          xxcameronjohnson@outlook.com
+        </a>
+        or through this form.
+      </p>
+      <form className="mt-10 flex flex-col">
+        <input
+          type="email"
+          className="h-14 rounded-lg borderBlack px-4"
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+        />
+        <button
+          type="submit"
+          className="group flex gap-2 items-center h-[3rem] w-[8rem] bg-gray-800 text-white rounded-full justify-center outline-none transition focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
+        >
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </button>
+      </form>
     </motion.section>
   );
 }
